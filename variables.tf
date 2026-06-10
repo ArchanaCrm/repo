c7e0e48f6a0fa9d41296807b47167f0ab94825ed
Chiyyavaram <archana.chiyyavaram@capgemini.com>
2026-06-10 13:46:25 -0500

    Updated Stop ec2 instances workflow

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "instance_type" {
   type = string
-  default: "t2.micro"
+  default = "t2.micro"
 }
 
 variable "instance_name" {
   type = string
-  default: "Port-EC2"
+  default = "Port-EC2"
 }
